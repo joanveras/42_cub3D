b8d2d16e1730521e7431231a337b1b235cceb736
joan veras <verasjoan587@gmail.com>
2025-04-21 17:26:39 -0300

    Fixed: no player error

--- sources/utils/file_validations/check_for_duplicates.c
+++ sources/utils/file_validations/check_for_duplicates.c
@@ -6,34 +6,54 @@
 /*   By: jveras <jveras@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 03:22:55 by jveras            #+#    #+#             */
-/*   Updated: 2025/04/21 03:58:30 by jveras           ###   ########.fr       */
+/*   Updated: 2025/04/21 17:13:31 by jveras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/cube3d.h"
 
-static void	check_for_duplicate_positions(t_program *program, int *counts)
+static int	check_remaining_positions(t_program *program, int *counts, int i)
 {
-	int i;
-
-	i = 0;
 	while (i < 4)
 	{
 		if (counts[i] > 0)
 		{
-			if (counts[i] > 1)
-				error_message(program, "Duplicated player start positions in map");
-			i++;
-			break;
+			return (1);
 		}
 		i++;
 	}
-	while (i < 4)
+	return (0);
+}
+
+static void	check_equal_character_doubles(t_program *program,
+	int *counts, int *i)
+{
+	while ((*i) < 4)
 	{
-		if (counts[i] > 0)
-			error_message(program, "Duplicated player start positions in map");
-		i++;
+		if (counts[*i] > 0)
+		{
+			if (counts[*i] > 1)
+			{
+				error_message(program,
+					"Duplicated player start positions in map");
+			}
+			(*i)++;
+			return ;
+		}
+		(*i)++;
 	}
+}
+
+static void	check_for_duplicate_positions(t_program *program, int *counts)
+{
+	int	i;
+
+	i = 0;
+	check_equal_character_doubles(program, counts, &i);
+	if (i == 4)
+		error_message(program, "There is no player start position in map");
+	else if (check_remaining_positions(program, counts, i))
+		error_message(program, "Duplicated player start positions in map");
 }
 
 static void initialize_counts(int *counts)
