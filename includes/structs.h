4425d9aac01bf410d7288517b58183a4d4c6f918
joan veras <verasjoan587@gmail.com>
2025-04-23 03:40:14 -0300

    Updated structs.h to introduce new structures for texture data and ceiling/floor management, refactored existing floor and ceiling color structure, and adjusted program structure to accommodate these changes.

--- includes/structs.h
+++ includes/structs.h
@@ -6,7 +6,7 @@
 /*   By: jveras <jveras@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 17:58:30 by marcribe          #+#    #+#             */
-/*   Updated: 2025/04/21 16:12:03 by jveras           ###   ########.fr       */
+/*   Updated: 2025/04/23 00:58:23 by jveras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,43 @@ typedef enum e_hook_events
 	ON_DESTROY = 17
 }	t_hook_events;
 
-typedef struct s_floor_and_ceiling_colors
+typedef struct s_texture_data
 {
-	uint64_t	floor_color;
-	uint64_t	ceiling_color;
-}	t_floor_and_ceiling_colors;
+	void	*tex_ptr;
+	int		width;
+	int		height;
+	int		bpp;
+	int		size_line;
+	int		endian;
+	char	*data;
+}	t_texture_data;
+
+typedef struct s_casting
+{
+	int		p;
+	int		cellX;
+	int		cellY;
+	int		tx;
+	int		ty;
+	float	rayDirX0;
+	float	rayDirY0;
+	float	rayDirX1;
+	float	rayDirY1;
+	float	posZ;
+	float	rowDistance;
+	float	floorStepX;
+	float	floorStepY;
+	float	floorX;
+	float	floorY;
+}	t_casting;
+
+typedef	struct s_ceilling_floor
+{
+	int				c_color;
+	int				f_color;
+	t_casting		casting;
+	t_texture_data	c_f_textures[2];
+}	t_ceilling_floor;
 
 typedef struct s_textures
 {
@@ -45,22 +77,11 @@ typedef struct s_img_data
 	void	*img_ptr;
 	int		width;
 	int		height;
-	char	*data;
 	int		bpp;
 	int		size_line;
 	int		endian;
+	char	*data;
 }	t_img_data;
-
-typedef struct s_texture_data
-{
-	void	*tex_ptr;
-	int		width;
-	int		height;
-	char	*data;
-	int		bpp;
-	int		size_line;
-	int		endian;
-}	t_texture_data;
 
 typedef struct s_player
 {
@@ -105,16 +126,16 @@ typedef struct s_map
 
 typedef struct s_program
 {
-	void						*mlx;
-	void						*mlx_win;
-	int							texture_index;
-	t_floor_and_ceiling_colors	f_c_colors;
-	t_textures					textures;
-	t_texture_data				wall_texture[4];
-	t_img_data					main_image;
-	t_player					player;
-	t_raycasting				raycast;
-	t_map						map;
+	void				*mlx;
+	void				*mlx_win;
+	int					texture_index;
+	t_ceilling_floor	ceilling_floor;
+	t_textures			textures;
+	t_texture_data		wall_texture[4];
+	t_img_data			main_image;
+	t_player			player;
+	t_raycasting		raycast;
+	t_map				map;
 }	t_program;
 
 #endif
